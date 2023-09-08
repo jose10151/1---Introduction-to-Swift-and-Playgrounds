bec596688ca787480d64c4c46ec39938bc549902
Jose Trujillo <joseh.trujillo11@gmail.com>
2023-09-08 13:36:02 -0400

    update

--- Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,12 +10,12 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("FireWork By Katie Perry")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print
 
 
 /*:
